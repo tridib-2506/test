5202f218cd6041aabf3d35bc770d032167e96bf8
tridib-2506 <tridib.nandi25062002@gmail.com>
2024-07-26 14:46:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -14,17 +14,15 @@ def home():
 @app.route('/scrape')
 def scrape():
     chrome_options = Options()
+    options.binary_location = '/opt/headless-chromium'
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--remote-debugging-port=9222')
 
-    # Set the path to the Chrome binary
-    chrome_options.binary_location = "/usr/bin/google-chrome"
-
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome('/opt/chromedriver', service=service, options=chrome_options)
 
     try:
         driver.get("https://www.google.com")
