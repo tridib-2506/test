618be75549c62e2027919f8babd93fc672dd28d5
tridib-2506 <tridib.nandi25062002@gmail.com>
2024-07-26 13:16:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
+from flask import Flask, jsonify
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Selenium is working"
+
 @app.route('/scrape')
 def scrape():
     chrome_options = Options()
